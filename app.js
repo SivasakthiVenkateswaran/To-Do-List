4acd3a945b311fa9b5fda47561b2b4900c8403f6
Siva Sakthi <sivasakthiv.22cse@kongu.edu>
2025-01-31 14:56:20 +0530

    new file

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 
+var inp = document.getElementById("input");
+var ui = document.getElementById("list-container");
+
+function add() {
+    var todo = document.createElement("li");
+    todo.innerHTML = inp.value + "<button onclick='delet(event)'>Delete</button>";
+    ui.append(todo);
+}
+
+function delet(event) {
+    event.target.parentElement.remove();
+}
